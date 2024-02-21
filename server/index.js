6fb9ce8581af78885eb0e285c6bf8c1bdce2453d
shaibaz-02 <shaibazmd916@gmail.com>
2024-02-21 22:27:45 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// const express=require('express');
+// const bodyParser=require('body-parser'); 
+const {Server, Socket}=require('socket.io');
+const io=new Server()
+io.on("connection",(Socket)=>{
+    console.log(`socket connected`,Socket.id)
+})
+// const app=express();
+
+// app.get('/',(req,res)=>{
+//     res.send("hello guys!!")
+// })
+
+// app.listen(8000,()=>{
+//     console.log("port 8000 is started");
+// })
