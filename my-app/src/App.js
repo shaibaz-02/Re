04e2cffce30013f6eb41375345a7acc4c863461a
shaibaz-02 <shaibazmd916@gmail.com>
2024-03-11 17:57:14 +0530

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { Routes,Route } from 'react-router-dom';
+import LobbyScreen from './screens/lobby';
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<LobbyScreen/>}/>
+      </Routes>
+    </div>
   );
 }
 
